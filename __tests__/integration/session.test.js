54cf7c36c9f373e99b3af4c3b885b2861da36bfc
joaohiago01 <joaohiago96@gmail.com>
2020-07-16 12:35:07 -0300

    Setting up test environment

---
+++ __tests__/integration/session.test.js
@@ -0,0 +1,11 @@
+const { User } = require('../../src/app/models')
+
+describe('Authentication', () => {
+    it('should authenticate with valid credentials', async () => {
+        
+    })
+
+    it('should receive JWT token when authenticated with valid credentials', async () => {
+        
+    })
+})
